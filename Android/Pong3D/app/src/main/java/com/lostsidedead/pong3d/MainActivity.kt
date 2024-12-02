fed5803a57b9338adbe93becf7b99f7e40ea3f6d
Jared Bruni <jaredbruni@gmail.com>
2024-12-02 15:44:00 -0800

    Pong3D
    Pong3D

---
+++ Android/Pong3D/app/src/main/java/com/lostsidedead/pong3d/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.lostsidedead.pong3d
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.ActivityInfo
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main) // Correct usage for XML layout
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        val webView: WebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://lostsidedead.biz/3DPong")
+    }
+}
